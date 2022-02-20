e7318ca677d88d2fa52ee211b0760af5e453881d
Jenna Qian <jiayin1996@gmail.com>
2022-02-20 17:12:26 -0500

    Installed dependencies and store name and favicon

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,11 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-const API = process.env.REACT_APP_API_URL;
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar.js"
 
-console.log(API);
 function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => {
-          setDays(response.data);
-        },
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
+ 
   return (
     <div>
-      <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul>
+      <h1>HI</h1>
     </div>
   );
 }
--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,5 +1,7 @@
 export default function Navbar () {
     return(
-        <div></div>
+        <div>
+            
+        </div>
     )
 }
