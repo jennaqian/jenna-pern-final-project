bd16dd9704157f31ce598adc990ff71f0c509e21
Jenna Qian <jiayin1996@gmail.com>
2022-02-22 19:57:21 -0500

    updated images in seed data

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -9,9 +9,11 @@ CREATE TABLE candles (
     id SERIAL PRIMARY KEY, 
     name TEXT NOT NULL,
     price INTEGER DEFAULT 0,
+    CHECK (price <= 10),
     scent TEXT,
     image TEXT,
-    rating INTEGER DEFAULT 0,
+    rating INTEGER DEFAULT 0, 
+    CHECK (rating > 0 AND rating < 6),
     featured BOOLEAN,
     description TEXT
 );
